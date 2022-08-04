d90f4dfd9406def75abf4274332bc174a5cf7aff
Konstantin Gavrilov <konstantin.gavrilov@medm.com>
2022-08-05 00:21:13 +0200

    Add flash

--- spec/requests/signup_requests_spec.rb
+++ spec/requests/signup_requests_spec.rb
@@ -13,9 +13,27 @@ RSpec.describe "SignupRequests", type: :request do
           password_confirmation: "bar"
         }
       }
-    end.to_not change{ User.count }
+    end.to_not change { User.count }
     assert_template 'users/new'
     assert_select 'div#error_explanation'
     assert_select 'div.alert-danger'
+  end
+
+  it "should sign up with valid params" do
+    get signup_path
+    expect(response).to have_http_status(200)
+    expect do
+      post users_path, params: {
+        user: {
+          name: "Ivan Petrovich",
+          email: "ivan.p@test.com",
+          password: "test_password",
+          password_confirmation: "test_password"
+        }
+      }
+    end.to change { User.count }.from(0).to(1)
+    follow_redirect!
+    assert_template 'users/show'
+    expect(flash).not_to be_empty
   end
 end
