f45b821b86cd296c1386e05c29b79f5d07b476ce
Konstantin Gavrilov <konstantin.gavrilov.92@gmail.com>
2022-09-01 22:02:22 +0200

    Update to rails 6.1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # Log the user in and redirect to user show page
+      log_in(user)
+      redirect_to user
     else
       # Render error message
       flash.now['danger'] = 'Incorrect email/password combination'
