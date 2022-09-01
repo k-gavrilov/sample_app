c95366548b9847a3dfdc1620877fe81e832a2885
Konstantin Gavrilov <konstantin.gavrilov.92@gmail.com>
2022-09-01 22:12:57 +0200

    Update to rails 7.0

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_04_074358) do
+ActiveRecord::Schema[6.1].define(version: 2021_12_04_074358) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
