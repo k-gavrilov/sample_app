309c83589259b05fa255f2edf2fe2ef0ece71354
Konstantin Gavrilov <konstantin.gavrilov.92@gmail.com>
2023-01-10 23:51:03 +0100

    Add tests for remember_me

--- spec/requests/users_logins_spec.rb
+++ spec/requests/users_logins_spec.rb
@@ -1,6 +1,13 @@
 require 'rails_helper'
+require 'requests/shared/session_test_helper'
+
+RSpec.configure do |c|
+  c.include SessionTestHelper
+end
 
 RSpec.describe "UsersLogins", type: :request do
+  let(:user) { create(:user, password: 'password', password_confirmation: 'password') }
+
   it "login with invalid information" do
     get login_path
     assert_template 'sessions/new'
@@ -8,5 +15,16 @@ RSpec.describe "UsersLogins", type: :request do
     expect(flash).not_to be_empty
     get root_path
     expect(flash).to be_empty
+  end
+
+  it 'should remember user when remember_me is set' do
+    log_in_as(user, remember_me: 1)
+    expect(cookies[:remember_token]).not_to be_nil
+  end
+
+  it 'should forget remember token upon signing in without remember_me'do
+    log_in_as(user, remember_me: 1)
+    log_in_as(user, remember_me: 0)
+    expect(cookies[:remember_token].blank?).to be_truthy
   end
 end
